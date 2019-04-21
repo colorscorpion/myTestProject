5a78446ddd642d6eae3dc7ebcddbed10f7d79465
colorscorpion <colorscorpion@gmail.com>
2019-04-21 13:20:17 -0400

    Modify Welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Laravel! - by Albert
                 </div>
 
                 <div class="links">
